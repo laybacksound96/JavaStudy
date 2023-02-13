dfdd134ca83971e613d6b996c542d5930b47c179
412 <laybacksound96@gmail.com>
2023-02-13 13:05:03 +0900

    Update For01.java

--- FirstProject/src/for01/For01.java
+++ FirstProject/src/for01/For01.java
@@ -21,17 +21,18 @@ public class For01 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         For01 calc = new For01();
-        
+
         System.out.println("Start Calculating. Write a first operand:");
         int operand1 = sc.nextInt();
         System.out.println("Next step. Write a operator:");
         String operator = sc.next();
         System.out.println("Final step. Write a second operand11:");
         int operand2 = sc.nextInt();
-        
-        for (int i = 0; i <= 2;i++){
-            System.out.println(i+"st result : "+calc.calc(operand1, operand2, operator));
+
+        for (int i = 0; i <= 2; i++) {
+            System.out.println(i + "st result : " + calc.calc(operand1, operand2, operator));
         }
         sc.close();
     }
+
 }
