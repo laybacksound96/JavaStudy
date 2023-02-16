389c330b7557a79364ff6feafc885d966b7fcbec
412 <laybacksound96@gmail.com>
2023-02-16 11:26:16 +0900

    Class

---
+++ FirstProject/src/day07/Object01.java
@@ -0,0 +1,9 @@
+package day07;
+
+public class Object01 {
+    int num = 10;
+    String name = "String";
+    String tel = "02-000-0000";
+    String adress = "서울시 영등포구 당산동 이레빌딩 19층";
+    
+}
---
+++ FirstProject/src/day07/index.java
@@ -0,0 +1,14 @@
+package day07;
+
+
+public class index {
+    public static void main(String[] args) {
+        Object01 ob1 = new Object01();
+        System.out.println(ob1.name);
+        ob1.tel = "02-3333-3333";
+
+        System.out.println(ob1.adress + "," + ob1.num);
+        System.out.println(ob1.adress="주소");
+        
+    }
+}
