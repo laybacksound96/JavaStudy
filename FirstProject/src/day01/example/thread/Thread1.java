bffb10e8d1b6d596a61f2ef75ec995db49539f12
412 <laybacksound96@gmail.com>
2023-03-14 11:26:54 +0900

    실습문제 풀이(배수 Thread)

--- FirstProject/src/day01/example/thread/Thread1.java
+++ FirstProject/src/day01/example/thread/Thread1.java
@@ -1,73 +1,69 @@
 package day01.example.thread;
 
+import java.util.Scanner;
+
 public class Thread1 {
     public static void main(String[] args) {
         Thread1 thread1 = new Thread1();
-        thread1.test3();
+        thread1.test2();
     }
 
     private void test2() {
-        Thread th1 = new MyThread1();
-        Thread th2 = new MyThread2();
+        Scanner scanner = new Scanner(System.in);
+        Thread th1 = new MyThread1(2,1);
+        Thread th2 = new MyThread1(3, 2);
+        
         th1.start();
         th2.start();
-    }
-
-    private void test3() {
-
-        String[] arr = { "A", "B", "C", "D", "E", "F", "G" };
-        for (String element : arr) {
-            new Thread(new Foo(element)).start();
+        
+        while (true) {
+            String line = scanner.nextLine();
+            if (line == "") {
+                th1.interrupt();
+                th2.interrupt();
+                System.out.println("실행종료됨");
+                break;
+            }
         }
+        scanner.close();
+        return;
     }
+
 
     class MyThread1 extends Thread {
+        int multipleArg;
+        int secToWait;
+
+        MyThread1(int multipleArg, int secToWait) {
+            this.multipleArg = multipleArg;
+            this.secToWait = secToWait;
+        }
         @Override
         public void run() {
             // TODO Auto-generated method stub
             super.run();
-            a("a");
-        }
-    }
-
-    class Foo implements Runnable {
-        private String str;
-
-        public Foo(String str) {
-            this.str = str;
-        }
-
-        public void run() {
-            for (int i = 0; i < 300; i++) {
-                System.out.print(str);
+            try {
+                multiplier(multipleArg,secToWait);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
             }
         }
     }
 
 
-    class MyThread2 extends Thread {
-        @Override
-        public void run() {
-            // TODO Auto-generated method stub
-            super.run();
-            b("b");
+    public void multiplier(int param, int waitSec) throws InterruptedException {
+        try {
+            for (int i = 1;; i++) {
+                int result = i * param;
+                System.out.println(waitSec+"초마다 실행됨("+param+"의 배수"+")"+result);
+                Thread.sleep(waitSec *1000);
+            }
+        } catch (InterruptedException  e) {
+            // TODO: handle exception
+            Thread.currentThread().interrupt();
         }
-    }
-
-    private void test1() {
-        a("a");
-        b("b");
+        
     }
     
-    public void a(String str) {
-        for (int i = 0; i < 300; i++) {
-            System.out.println(str);
-        }
-    }
-
-    public void b(String str) {
-        for (int i = 0; i < 300; i++) {
-            System.out.println(str);
-        }
-    }
 }
